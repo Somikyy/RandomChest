94c9dcd28f32096992ee1c32346bb5f6c286a926
Somikyy <somi.kyy@mail.ru>
2024-05-16 13:13:32 +0200

    initial commit
    initial commit

---
+++ src/main/java/org/randomchest/RandomChest.java
@@ -0,0 +1,17 @@
+package org.randomchest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomChest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
